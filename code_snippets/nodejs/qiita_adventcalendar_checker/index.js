0dd0f97be05374bb56b7577302fd4f0a3d4d94c1
Ryota Murohoshi <ryotamurohoshi@gmail.com>
2016-01-11 14:00:47 +0900

    use bluebird

--- code_snippets/nodejs/qiita_adventcalendar_checker/index.js
+++ code_snippets/nodejs/qiita_adventcalendar_checker/index.js
@@ -1,13 +1,24 @@
 var Xray = require('x-ray');
+var Promise = require("bluebird");
 
 var url = "http://qiita.com/advent-calendar/2015/unity";
 var x = Xray();
 
-x(url, ".adventCalendarCalendar_day",
-    [{
-        day: ".adventCalendarCalendar_date",
-        item_url: ".adventCalendarCalendar_comment a@href"
-    }])
-    (function (error, result) {
-        console.log(result);
-    })
+function crawlTargetCalendar(calendarUrl) {
+    return new Promise(function (resolve, reject) {
+        x(calendarUrl, ".adventCalendarCalendar_day",
+            [{
+                day: ".adventCalendarCalendar_date",
+                item_url: ".adventCalendarCalendar_comment a@href"
+            }])
+            (function (error, result) {
+                if (error) {
+                    reject(error);
+                } else {
+                    resolve(result);
+                }
+            })
+    });
+}
+
+crawlTargetCalendar(url).then(console.log);
