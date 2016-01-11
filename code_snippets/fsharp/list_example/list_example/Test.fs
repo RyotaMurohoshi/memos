03c4bf5a047b4ea851d0c9150b6bd8abbf033f3a
Ryota Murohoshi <ryotamurohoshi@gmail.com>
2016-01-11 21:54:04 +0900

    added list example

---
+++ code_snippets/fsharp/list_example/list_example/Test.fs
@@ -0,0 +1,10 @@
+﻿namespace list_example
+open System
+open NUnit.Framework
+
+[<TestFixture>]
+type Test() = 
+
+    [<Test>]
+    member x.TestMap() =
+        Assert.AreEqual([1; 2; 3; 4; 5] |> List.map (fun n -> n * 2), [2; 4; 6; 8; 10])
