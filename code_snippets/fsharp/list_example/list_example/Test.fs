ee5fea553fb7d56b8f15f59fa12507588c50f09c
Ryota Murohoshi <ryotamurohoshi@gmail.com>
2016-01-12 23:36:05 +0900

    added fsharp

--- code_snippets/fsharp/list_example/list_example/Test.fs
+++ code_snippets/fsharp/list_example/list_example/Test.fs
@@ -5,10 +5,24 @@ open NUnit.Framework
 [<TestFixture>]
 type Test() = 
 
+    let double n = n * 2
+    let isEven n = n % 2 = 0
+
     [<Test>]
     member x.TestMap() =
-        Assert.AreEqual([1; 2; 3; 4; 5] |> List.map (fun n -> n * 2), [2; 4; 6; 8; 10])
+        Assert.AreEqual([1; 2; 3; 4; 5] |> List.map double, [2; 4; 6; 8; 10])
 
     [<Test>]
     member x.TestFilter() =
-        Assert.AreEqual([1; 2; 3; 4; 5] |> List.filter (fun n -> n % 2 = 0), [2; 4])
+        Assert.AreEqual([1; 2; 3; 4; 5] |> List.filter isEven, [2; 4])
+
+
+    [<Test>]
+    member x.TestForAll() =
+        Assert.IsFalse([1; 2; 3; 4; 5] |> List.forall isEven)
+        Assert.IsTrue([1; 2; 3; 4; 5] |> List.map double |> List.forall isEven)
+
+    [<Test>]
+    member x.TestExists() =
+        Assert.IsTrue([1; 2; 3; 4; 5] |> List.exists isEven)
+        Assert.IsFalse([1; 2; 3; 4; 5] |> List.exists (fun n -> n = 0))
