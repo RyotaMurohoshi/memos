0a1dd9aec8a4190f013a8ed1e10eb8e7d348e4c6
Ryota Murohoshi <ryotamurohoshi@gmail.com>
2016-01-15 00:25:18 +0900

    added executor project

---
+++ code_snippets/java/versiong_problems/executor/src/com/mrstar/executor/Executor.java
@@ -0,0 +1,9 @@
+package com.mrstar.executor;
+
+import com.mrstar.constants.Constant;
+
+public class Executor {
+    public void execute (){
+        System.out.println(Constant.MESSAGE);
+    }
+}
