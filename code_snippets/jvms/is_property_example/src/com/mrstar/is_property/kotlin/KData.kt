029010fbd7ed0bdba46ce3dbf44310d6a4babcc1
Ryota Murohoshi <ryotamurohoshi@gmail.com>
2016-01-18 00:00:36 +0900

    created files

---
+++ code_snippets/jvms/is_property_example/src/com/mrstar/is_property/kotlin/KData.kt
@@ -0,0 +1,7 @@
+package com.mrstar.is_property.kotlin
+
+class KData(var name:String,
+            var enabled0:Boolean,
+            var isEnabled1:Boolean,
+            var is_enabled2: Boolean,
+            var isenabled3: Boolean)
