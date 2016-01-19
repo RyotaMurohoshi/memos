6fb50a7ea4d84fe7366de40f2030cfc56a16c60d
Ryota Murohoshi <ryotamurohoshi@gmail.com>
2016-01-19 20:10:03 +0900

    added core library

---
+++ code_snippets/java/versiong_problems/core/src/com/mrstar/versioning_problem/core/Core.java
@@ -0,0 +1,12 @@
+package com.mrstar.versioning_problem.core;
+
+public final class Core {
+    private Core() {
+    }
+
+    public static String getTitle() {
+        return "ほげふが";
+    }
+
+    public final static String MESSAGE = "ぴよぴよ！";
+}
