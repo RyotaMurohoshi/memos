18fd4b4135bb862c7df657c792262408508b7dca
Ryota Murohoshi <ryotamurohoshi@gmail.com>
2016-02-03 23:22:03 +0900

    added array example

--- code_snippets/nodejs/xray_example/index.js
+++ code_snippets/nodejs/xray_example/index.js
@@ -4,14 +4,23 @@ let x = Xray();
 
 let url = 'http://qiita.com/RyotaMurohoshi/items';
 
-{
-    let selector = '#main > div > div > div.col-md-3.col-sm-3.col-xs-12.newUserPageProfile > div > div.col-xs-8.col-sm-12 > h3';
-
-    x(url, selector)((error, result) => {
-        if (error) {
-            console.log(error);
-        } else {
-            console.log(result);
-        }
-    });
+function show(error, result) {
+    if (error) {
+        console.log(error);
+    } else {
+        console.log(result);
+    }
 }
+
+function simpleExample() {
+    let selector = '#main > div > div > div.col-md-3.col-sm-3.col-xs-12.newUserPageProfile > div > div.col-xs-8.col-sm-12 > h3';
+    x(url, selector)(show);
+}
+
+function arrayExample() {
+    let selector = ['a@href'];
+    x(url, selector)(show);
+}
+
+
+arrayExample();
